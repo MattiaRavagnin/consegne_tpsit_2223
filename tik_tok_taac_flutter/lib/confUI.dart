e0ee8ba3e85cd1de798f764feaea73e88f31e1f1
MattiaRavagnin <71636249+MattiaRavagnin@users.noreply.github.com>
2022-10-19 13:34:17 +0200

    create ColorConf, start code in  main.dart

---
+++ tik_tok_taac_flutter/lib/confUI.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ConfColor {
+  static Color black = Color.fromARGB(0, 1, 1, 1);
+  static Color seaSerpent = Color.fromARGB(0, 105, 201, 208);
+  static Color white = Color.fromARGB(0, 255, 255, 255);
+  static Color redCrayola = Color.fromARGB(0, 238, 29, 82);
+}
--- tik_tok_taac_flutter/lib/main.dart
+++ tik_tok_taac_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tik_tok_taac_flutter/confUI.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,8 +24,42 @@ class GameScreen extends StatefulWidget {
 }
 
 class _GameScreenState extends State<GameScreen> {
+  String lastValue = "X";
+  bool gameOver = false;
+  int turn = 0;
+  String result = "";
+
+  List<int> scoreboard = [0, 0, 0, 0, 0, 0, 0, 0];
+
+  // Game game = Game(); classe dart per la logica di gioco
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double boardWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: ConfColor.white,
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "It's ${lastValue} turn".toUpperCase(),
+              style: TextStyle(
+                color: ConfColor.black,
+                fontSize: 58,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            // https://github.com/doctorcode9/Flutter-Tic-Tact-Toe-Game
+            // TODO: make game class that will contains all data and method
+          ]),
+    );
   }
 }
