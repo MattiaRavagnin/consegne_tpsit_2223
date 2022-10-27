a3af746929e88ae39f8339719cef12748988b676
rava0n <mattiaravagnin04@gmail.com>
2022-10-27 21:32:00 +0200

    update game

---
+++ tik_tok_taac_flutter/lib/pages/Game1.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import 'package:tik_tok_taac_flutter/game.dart';
+import 'package:tik_tok_taac_flutter/confUI.dart';
+import 'package:tik_tok_taac_flutter/pages/Home.dart';
+
+import 'package:animated_background/animated_background.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({Key? key}) : super(key: key);
+
+  @override
+  _GameScreenState createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> with TickerProviderStateMixin {
+  String lastValue = "X";
+  bool gameOver = false;
+  int turn = 0;
+  String result = "";
+
+  List<int> scoreboard = [0, 0, 0, 0, 0, 0, 0, 0];
+
+  Game game = Game(); //classe dart per la logica di gioco
+
+  @override
+  void initState() {
+    super.initState();
+    game.board = Game.initGameBoeard();
+    print(game.board);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double boardWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      //backgroundColor: Colors.cyan,
+      body: AnimatedBackground(
+        behaviour: RacingLinesBehaviour(),
+        vsync: this,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "It's ${lastValue} turn".toUpperCase(),
+                style: TextStyle(
+                  color: ConfColor.black,
+                  fontSize: 58,
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+
+              Container(
+                width: boardWidth,
+                height: boardWidth,
+                child: GridView.count(
+                  crossAxisCount: Game.boardLenth ~/ 3,
+                  padding: EdgeInsets.all(16.0),
+                  mainAxisSpacing: 8.0,
+                  crossAxisSpacing: 8.0,
+                  children: List.generate(Game.boardLenth, (index) {
+                    return InkWell(
+                      onTap: gameOver
+                          ? null
+                          : () {
+                              //refresh the screen
+                              //button repeat game
+
+                              if (game.board![index] == '') {
+                                setState(() {
+                                  game.board![index] = lastValue;
+
+                                  turn++;
+
+                                  gameOver =
+                                      game.win(lastValue, index, scoreboard, 3);
+
+                                  if (gameOver) {
+                                    result = '$lastValue WIN';
+                                  } else if (!gameOver && turn == 9) {
+                                    result = 'DRAW';
+                                    gameOver = true;
+                                  }
+
+                                  if (lastValue == 'X') {
+                                    lastValue = 'O';
+                                  } else {
+                                    lastValue = 'X';
+                                  }
+                                });
+                              }
+                            },
+                      child: Container(
+                        width: Game.sizeBloc,
+                        height: Game.sizeBloc,
+                        decoration: BoxDecoration(
+                            color: Colors.black, //???>?
+                            borderRadius: BorderRadius.circular(10.0)),
+                        child: Center(
+                          child: Text(
+                            game.board![index],
+                            style: TextStyle(
+                                color: game.board![index] == 'X'
+                                    ? Colors.amberAccent
+                                    : Colors.deepOrange,
+                                fontSize: 64.0,
+                                fontFamily: 'Raleway'),
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+                ),
+              ),
+              SizedBox(
+                height: 25.0,
+              ),
+              Text(
+                result,
+                style: TextStyle(color: ConfColor.black, fontSize: 54.0),
+              ),
+              ElevatedButton.icon(
+                onPressed: () {
+                  setState(() {
+                    //reset
+                    game.board = Game.initGameBoeard();
+                    lastValue = 'X';
+                    gameOver = false;
+                    turn = 0;
+                    result = '';
+                    scoreboard = [0, 0, 0, 0, 0, 0, 0, 0];
+                  });
+                },
+                icon: Icon(Icons.replay),
+                label: Text('RESTART'),
+                style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.all(10.0),
+                    fixedSize: Size(100, 40),
+                    textStyle:
+                        TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                    primary: Colors.yellow,
+                    onPrimary: Colors.black,
+                    elevation: 15,
+                    shadowColor: Colors.yellow),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton.icon(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => HomePage()),
+                  );
+                },
+                icon: Icon(Icons.home),
+                label: Text('HOME'),
+                style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.all(10.0),
+                    fixedSize: Size(100, 40),
+                    textStyle:
+                        TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                    primary: Colors.yellow,
+                    onPrimary: Colors.black,
+                    elevation: 15,
+                    shadowColor: Colors.yellow),
+              ),
+
+              // https://github.com/doctorcode9/Flutter-Tic-Tact-Toe-Game
+              // TODO: make game class that will contains all data and method
+            ]),
+      ),
+    );
+  }
+}
---
+++ tik_tok_taac_flutter/lib/pages/Home.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:tik_tok_taac_flutter/pages/Game1.dart';
+
+import 'package:animated_background/animated_background.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tik Tok Taac'),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+      ),
+      body: AnimatedBackground(
+          behaviour: RacingLinesBehaviour(),
+          vsync: this,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  child: Text('1 vs 1'),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => GameScreen()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.all(20.0),
+                      fixedSize: Size(300, 80),
+                      textStyle:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                      primary: Colors.yellow,
+                      onPrimary: Colors.black,
+                      elevation: 15,
+                      shadowColor: Colors.yellow),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                  child: Text('1 vs IA'),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => GameScreen()),
+                    ); //inserire classe con la "IA"
+                  },
+                  style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.all(20.0),
+                      fixedSize: Size(300, 80),
+                      textStyle:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                      primary: Colors.yellow,
+                      onPrimary: Colors.black,
+                      elevation: 15,
+                      shadowColor: Colors.yellow),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
