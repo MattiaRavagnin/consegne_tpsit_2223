ecfbb5125e420a9cb85e79cff8e6f3b7bc37ac70
rava0n <mattiaravagnin04@gmail.com>
2022-11-08 20:54:27 +0100

    finish random / ia

--- tik_tok_taac_flutter/lib/pages/Home.dart
+++ tik_tok_taac_flutter/lib/pages/Home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:animated_background/animated_background.dart';
 import 'package:tik_tok_taac_flutter/pages/game/game_page.dart';
+import 'package:tik_tok_taac_flutter/pages/game/game_page_random.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -48,11 +49,31 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                   height: 30,
                 ),
                 ElevatedButton(
-                  child: Text('1 vs IA'),
+                  child: Text('1 vs IA Easy'),
                   onPressed: () {
-                    //Navigator.of(context).push(
-                    //  MaterialPageRoute(builder: (context) => ),
-                    //); //inserire classe con la "IA"
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => GamePageRandom()),
+                    ); //inserire classe con la "IA"
+                  },
+                  style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.all(20.0),
+                      fixedSize: Size(300, 80),
+                      textStyle:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                      primary: Colors.yellow,
+                      onPrimary: Colors.black,
+                      elevation: 15,
+                      shadowColor: Colors.yellow),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                  child: Text('1 vs IA Impossible'),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => GamePageRandom()),
+                    ); //inserire classe con la "IA"
                   },
                   style: ElevatedButton.styleFrom(
                       padding: EdgeInsets.all(20.0),
